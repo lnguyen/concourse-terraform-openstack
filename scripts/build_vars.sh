1d92b9ecefbfd5dbfbafab60234d41955c1e4945
Long Nguyen <long.nguyen11288@gmail.com>
2015-05-18 09:37:16 -0400

    added building terraform vars

---
+++ scripts/build_vars.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+
+cat <<EOF > terraform.vars
+network = "192.168"
+auth_url="${AUTH_URL}"
+tenant_name="${TENANT_NAME}"
+tenant_id="${TENANT_ID}"
+username="${OPENSTACK_USERNAME}"
+password="${OPENSTACK_PASSWORD}"
+public_key_path="/Users/lnguyen/.ssh/do.pub"
+key_path="/Users/lnguyen/.ssh/do"
+floating_ip_pool="${FLOATING_IP_POOL}"
+region="RegionOne"
+network_external_id="${NETWORK_EXTERNAL_ID}"
+install_docker_services="true"
+EOF
