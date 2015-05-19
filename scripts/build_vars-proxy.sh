ce31d795f4f0e032b3a003d0c83f64b2370e9a63
Long Nguyen <long.nguyen11288@gmail.com>
2015-05-19 09:47:19 -0400

    added script to start testing proxy

---
+++ scripts/build_vars-proxy.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+
+cat <<EOF > terraform.tfvars
+network = "192.168"
+auth_url="${AUTH_URL}"
+tenant_name="${TENANT_NAME}"
+tenant_id="${TENANT_ID}"
+username="${OPENSTACK_USERNAME}"
+password="${OPENSTACK_PASSWORD}"
+public_key_path="fix-ssh-key-permission/robots.pub"
+key_path="fix-ssh-key-permission/robots"
+floating_ip_pool="${FLOATING_IP_POOL}"
+region="RegionOne"
+network_external_id="${NETWORK_EXTERNAL_ID}"
+install_docker_services="true"
+http_proxy="${HTTP_PROXY}"
+https_proxy="${HTTPS_PROXY}"
+EOF
